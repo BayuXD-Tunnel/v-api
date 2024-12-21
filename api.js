85622317a2764086e7560ebfbc9d94308852a65e
BayuXD-Tunnel <bayuriadi82@smp.belajar.id>
2024-12-21 14:26:07 +0700

    Update api.js

--- api.js
+++ api.js
@@ -411,20 +411,6 @@ return res.status(200).json({
 status: true,
 data: response.data.data,
 });
-} else if (true) { // XNXXDL
-const response = await axios.get(`https://api.bayuxd.tech/api/orkut/createpayment?apikey=${apikey}&amount=${amount}&codeqr=${codeqr}`
-);
-return res.status(200).json({
-status: true,
-data: response.data.data,
-});
-} else if (s === 'checkpayment') { // XNXXDL
-const response = await axios.get(`https://api.bayuxd.tech/api/orkut/checkpayment?apikey=KontolPremium&merchant=${merchant}&token=${token}`
-);
-return res.status(200).json({
-status: true,
-data: response.data.data,
-});
 
 // ANIME MENU
 } else if (s === 'otakudesu-src') { // OTKD-SRC
